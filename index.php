cfd76a0c382ec37f0b2e8c67c7d4f7ce516c6640
Marty Bonacci <martybonacci@gmail.com>
2017-07-14 08:38:32 -0600

    added Marty persona and use case

--- index.php
+++ index.php
@@ -9,10 +9,13 @@
 	<body>
 		<h1>Data Design Assignment Based on Etsy Product Page</h1>
 		<h2>Persona</h2>
-		<p>Matt Barbato is at his core, a cowboy. He trolls Etsy almost every day looking for unique handmade western leather accessories such as hand tooled belts, one of a kind cowboy boots, and bright colored rodeo chaps.</p>
+		<p>Marty Bonacci sells western belt an Native American style moccasins on ebay, Amazon and a stand alone website, NativeLeather.com. The products are made in Gallup, NM by Marty's friend, Tony Bonaguiti. This friend/business relationship has positioned Marty as the exclusive online seller that Tony will drop ship product for. Marty has observed that the more venues he offers product on, the more product sells.</p>
+		<!-- Probably will NOT use Matt<p>Matt Barbato is at his core, a cowboy. He trolls Etsy almost every day looking for unique handmade western leather accessories such as hand tooled belts, one of a kind cowboy boots, and bright colored rodeo chaps.</p>-->
 		<h2>Use Case</h2>
-		<p>Matt searches for terms such as "made in the usa" and "handmade leather" on Etsy. The search results have the terms in the itemName, itemOverview, and/or the itemDetails.</p>
+		<p>To start selling on Etsy, Marty will need to first setup a seller account, then list items for sale.</p>
+		<!--<p>Matt searches for terms such as "made in the usa" and "handmade leather" on Etsy. The search results have the terms in the itemName, itemOverview, and/or the itemDetails.</p>-->
 		<h2>Interaction Flow</h2>
+		<p></p>
 		<img src="images/data-design-etsy.svg" alt="data diagram">
 		<h2>Conceptual Model</h2>
 
