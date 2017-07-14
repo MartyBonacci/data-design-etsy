379916c6ada45de6992d37da4bcc15571aa690f4
Marty Bonacci <martybonacci@gmail.com>
2017-07-13 22:49:28 -0600

    create index.php

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="author" content="Marty Bonacci">
+		<meta name="description" content="data design assignment based on etsy product page">
+		<title>data design etsy</title>
+	</head>
+	<body>
+		<h1>Data Design Assignment Based on Etsy Product Page</h1>
+		<h2>Persona</h2>
+		<p>Matt Barbato is at his core, a cowboy. He trolls Etsy almost every day looking for unique handmade western leather accessories such as hand tooled belts, one of a kind cowboy boots, and bright colored rodeo chaps.</p>
+		<h2>Use Case</h2>
+		<p>Matt searches for terms </p>
+		<h2>Interaction Flow</h2>
+		<h2>Conceptual Model</h2>
+
+	</body>
+</html>
