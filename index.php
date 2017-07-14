2e1bd59bafae78671cf7a8add30d0e1fef441a5b
Marty Bonacci <martybonacci@gmail.com>
2017-07-14 09:21:29 -0600

    added interaction flow

--- index.php
+++ index.php
@@ -10,11 +10,17 @@
 		<h1>Data Design Assignment Based on Etsy Product Page</h1>
 		<h2>Persona</h2>
 		<p>Marty Bonacci sells western belt an Native American style moccasins on ebay, Amazon and a stand alone website, NativeLeather.com. The products are made in Gallup, NM by Marty's friend, Tony Bonaguiti. This friend/business relationship has positioned Marty as the exclusive online seller that Tony will drop ship product for. Marty has observed that the more venues he offers product on, the more product sells.</p>
-		<!-- Probably will NOT use Matt<p>Matt Barbato is at his core, a cowboy. He trolls Etsy almost every day looking for unique handmade western leather accessories such as hand tooled belts, one of a kind cowboy boots, and bright colored rodeo chaps.</p>-->
 		<h2>Use Case</h2>
 		<p>To start selling on Etsy, Marty will need to first setup a seller account, then list items for sale.</p>
-		<!--<p>Matt searches for terms such as "made in the usa" and "handmade leather" on Etsy. The search results have the terms in the itemName, itemOverview, and/or the itemDetails.</p>-->
 		<h2>Interaction Flow</h2>
+		<ol>
+			<li>The user will navigate to the "create a seller account" page on Etsy.</li>
+			<li>The user will enter the seller attributes, sellerShopName, sellerLocation, sellerOnEtsySince, sellerShopOwnerName to create a seller account</li>
+			<li>The user/seller will navigate to the "list an item for sale" page.</li>
+			<li>The user will enter the item attributes, itemName, itemPrice, itemDetails, itemOverview, itemShippingPolicies, itemReviews, itemQuantity</li>
+		</ol>
+
+		<h2>Entity Relationship Diagram</h2>
 		<img src="images/data-design-etsy.svg" alt="data diagram">
 		<h2>Conceptual Model</h2>
 		<h3>Entities and Attributes</h3>
